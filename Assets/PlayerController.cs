d92ced3ae03c10fdf8570f2f264a9403eb1f047a
20060519 <91940216+20060519@users.noreply.github.com>
2022-01-11 20:01:32 +0900

    スクリプトの設定

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -30,15 +30,14 @@ public class PlayerController : MonoBehaviour
         // 方向キーの入力値とカメラの向きから、移動方向を決定
         Vector3 moveForward = cameraForward * inputY + Camera.main.transform.right * inputX;
 
-
         moveForward *= speed;
-       
 
         if (moveForward.magnitude > 0.01f) //ベクトルの長さが0.01fより大きい場合にプレイヤーの向きを変える処理を入れる(0では入れないので）
         {
             Quaternion targetRotation = Quaternion.LookRotation(moveForward);  //ベクトルの情報をQuaternion.LookRotationに引き渡し回転量を取得しプレイヤーを回転させる
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime);
         }
+
         controller.Move(moveForward * Time.deltaTime);
         Player_pos = transform.position; //プレイヤーの位置を更新
     }
